0ee1e265197260a28fbb4489d0290063dc041ed3
vaibhavdesai <vaibhavdesai@icloud.com>
2017-12-19 15:28:00 -0700

    modification in the logic of possible places

--- BFS/BFS.py
+++ BFS/BFS.py
@@ -1,153 +1,114 @@
 import copy
 import time
 
-def isSafe(grid, row, col):
-    if [row,col] in tArray:
+def isLizardSafe(all_possible_lizards_positions, row_no, column_no):
+
+    if (row_no,column_no) in trees_coordinates:
         return False
-    for pair in grid:
-        if row == pair[0]:
-            is_blocking = False
-            for tree in tArray:
-                if pair[0] == tree[0] and pair[1] < tree[1] < col :
-                    is_blocking = True
-                    break
-            if not is_blocking:
+
+    row_move = [0, 1, 1, 1, 0, -1, -1, -1]
+    column_move = [-1, -1, 0, 1, 1,  1,  0, -1]
+    for i in range(8):
+        row_inc = row_move[i];
+        column_inc = column_move[i];
+
+        row_position = row_no + row_inc;
+        column_position = column_no + column_inc;
+
+        while (row_position >= 0 and row_position < size_of_nursery and column_position >= 0 and column_position < size_of_nursery and not ((row_position,column_position) in trees_coordinates)):
+            if (row_position,column_position) in all_possible_lizards_positions:
                 return False
-
-        if col == pair[1]:
-            is_blocking = False
-            if pair[0] > row:
-                for tree in tArray:
-                    if tree[1] == col and tree[0] in range(row+1,pair[0]):
-                        is_blocking = True
-                        break
-            else:
-                for tree in tArray:
-                    if tree[1] == col and tree[0] in range(pair[0]+1,row):
-                        is_blocking = True
-                        break
-
-            if not is_blocking:
-                return False
-
-    i = row+1
-    j = col+1
-    while i < n and j < n:
-        if [i,j] in grid:
-            return False
-        i += 1
-        j += 1
-
-    i = row-1
-    j = col+1
-    while -1 < i < n and -1 < j < n:
-        if [i,j] in tArray:
-            break
-        if [i,j] in grid:
-            return False
-        i -= 1
-        j += 1
-
-    i = row-1
-    j = col-1
-    while -1 < i < n and -1 < j < n:
-        if [i,j] in tArray:
-            break
-        if [i,j] in grid:
-            return False
-        i -= 1
-        j -= 1
-
-    i = row+1
-    j = col-1
-    while -1 < i < n and -1 < j < n:
-        if [i,j] in tArray:
-            break
-        if [i,j] in grid:
-            return False
-        i += 1
-        j -= 1
+            row_position += row_inc
+            column_position += column_inc
 
     return True
 
-def newGrid(grid, row, col):
-    new_grid = copy.deepcopy(grid)
-    new_grid.append([row,col])
-    return new_grid
+def newSet(lizards_position_set, row_no, col_no):
+    new_lizards_position_set = copy.deepcopy(lizards_position_set)
+    new_lizards_position_set.add((row_no,col_no))
+    return new_lizards_position_set
 
 
-def possiblePlaces(grid, col, r):
+def possiblePlaces(lizards_position_set,row_no, column_no):
     global count
     queue = []
-    #print grid
-    for row in range(r+1,n):
-        #print "ROwL",row
-        if isSafe(grid, row, col):
-            #print "col:",col, "row:",row
+    for row in range(row_no+1,size_of_nursery):
+        if isLizardSafe(lizards_position_set, row, column_no):
             count += 1
             #print count
-            new_grid = newGrid(grid, row, col)
-            #print new_grid
-            queue.append(new_grid)
-            '''print queue
-            print display(new_grid)
-            raw_input()'''
-            queue += possiblePlaces(new_grid,col,row)
-
-
+            new_lizards_position_set = newSet(lizards_position_set, row, column_no)
+            queue.append(new_lizards_position_set)
+            #print display(new_lizards_position_set)
+            #print queue
+            #raw_input()
+            queue += possiblePlaces(new_lizards_position_set, row, column_no)
 
     return queue
 
 
-def run(grid):
+def run(lizards_position_set):
+
     queue = []
-    for c in range(n):
-        queue = possiblePlaces(grid,c,-1)
+    no_solution = False
+    for start_column in range(size_of_nursery):
+        queue = possiblePlaces(lizards_position_set, -1, start_column)
+        #print queue
+        for position_set in queue:
+            if len(position_set) == no_of_lizards:
+                return True, position_set
         if len(queue) != 0:
             break
 
-    for col in range(c+1 ,n):
-        childQueue = []
-        #print queue
-        done = False
-        for grid in queue:
-            childQueue += possiblePlaces(grid, col,-1)
-            if len(childQueue) != 0 and col == n-1:
-                for childGrid in childQueue:
-                    if len(childGrid) == l:
-                        done = True
-                        print display(childGrid)
-                        break
-            if done:
-                break
+    for column in range(start_column+1, size_of_nursery):
+        child_queue = []
+        for lizards_position_set in queue:
+            updated_lizards_position_queue = possiblePlaces(lizards_position_set, -1, column)
+            #print updated_lizards_position_set
+            for position_set in updated_lizards_position_queue:
+                if len(position_set) == no_of_lizards:
+                    return True,position_set
+            child_queue += updated_lizards_position_queue
+        queue = child_queue
 
-        queue = childQueue
+    return no_solution,set()
 
-def display(grid):
-    for i in range(n):
-        for j in range(n):
-            if [i,j] in tArray:
-                print "2",
-            elif [i,j] in grid:
-                print "1",
+def display(position_set):
+    for i in range(size_of_nursery):
+        for j in range(size_of_nursery):
+            if (i,j) in trees_coordinates:
+                print("2"),
+            elif (i,j) in position_set:
+                print("1"),
             else:
-                print "0",
-        print "\n",
+                print("0"),
+        print("\n"),
+
+
 
 tstart = time.time()
-n = raw_input()
+
 file = open("input.txt","r")
-n = int(file.readline())
-l = int(file.readline())
-tArray = []
+algorithm_type = file.readline()
+size_of_nursery = int(file.readline())
+no_of_lizards = int(file.readline())
+
+
+trees_coordinates = set()
+
 count = 0
-for i in range(n):
-    row = [x for x in file.readline()]
-    print row
-    for index in range(len(row)-1):
-        if row[index] == '2':
-            tArray.append([i,index])
-print tArray
-print run([])
+for i in range(size_of_nursery):
+    row = file.readline()
+    for j in range(len(row)-1):
+        if row[j] == '2':
+            trees_coordinates.add((i,j))
+
+
+solution, position_set = run(set())
+
+if solution:
+    print("OK")
+    display(position_set)
+else:
+    print("Fail")
+
 print (time.time())-tstart
-print count
